51d8bb90c1947b55374b1b282494c7f968936d3b
mohi0 <88849405+mohi0@users.noreply.github.com>
2025-01-29 00:18:11 +0530

    fixedconvolution1D

--- convolution.js
+++ convolution.js
@@ -6,7 +6,9 @@ convolute1D(arr, karr){
        const seqIndex = i - j;
        if (seqIndex >= 0 && seqIndex < arr.length) {
           outputArray[i] += arr[seqIndex] * karr[j];
-       }
+       }else {
+          outputArray[i] += 0; // Explicit zero-padding
+        }
      }
   }
 }
